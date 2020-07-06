3d15abaea9dc5af6de42acf6c6aa513f36464d65
Q-digital <no-reply@q-digital.org>
2020-07-06 16:50:15 +0400

    stage

--- precache-manifest.17a04c2d060f0196c06d944d2c08c658.js
+++ precache-manifest.17a04c2d060f0196c06d944d2c08c658.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "df512b5574e0d461e99fc37cbd0d0bea",
+    "revision": "0ab05c68906e7d3452b67711abdef200",
     "url": "./index.html"
   },
   {
-    "revision": "76530959854cd7f9c1b5",
+    "revision": "410570d9abce11d5b7ec",
     "url": "./static/css/main.5e98a0d9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.c12d69d9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "76530959854cd7f9c1b5",
-    "url": "./static/js/main.3273d334.chunk.js"
+    "revision": "410570d9abce11d5b7ec",
+    "url": "./static/js/main.ce103f5a.chunk.js"
   },
   {
     "revision": "3de254941cded504f191",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c11e1356cb908095722c7d81b2c32cb4.js"
+  "./precache-manifest.17a04c2d060f0196c06d944d2c08c658.js"
 );
 
 self.addEventListener('message', (event) => {
