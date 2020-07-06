b1fb1702f12eb00442ed99f129d55f5b5d8d299d
Q-digital <no-reply@q-digital.org>
2020-07-06 10:52:26 +0400

    stage

---
+++ precache-manifest.4709179c4e7aae230b73364b2bf2583e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f841ed0b203eef7490cd6c7eb728d716",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c5042426e16f70b4cbc",
+    "url": "./static/css/main.5e98a0d9.chunk.css"
+  },
+  {
+    "revision": "17d77918e24f78b0a41e",
+    "url": "./static/js/2.45c6c308.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "./static/js/2.45c6c308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5042426e16f70b4cbc",
+    "url": "./static/js/main.391eb2d0.chunk.js"
+  },
+  {
+    "revision": "3de254941cded504f191",
+    "url": "./static/js/runtime-main.e37dd673.js"
+  }
+]);
